6234ed0cc31d78f5374dbd8bfd2d1b489f64caa8
DhruvKumar2917 <dhruvkumar6297@gmail.com>
2025-10-04 20:44:45 +0530

    last commit1

--- server/index.js
+++ server/index.js
@@ -27,7 +27,7 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(
   cors({
-    origin: "http://localhost:3000",
+    origin: "https://edtech-studynotion-three.vercel.app",
     credentials: true,
   })
 );
--- server/mail/templates/paymentSuccessEmail.js
+++ server/mail/templates/paymentSuccessEmail.js
@@ -67,7 +67,7 @@ exports.paymentSuccessEmail = (name, amount, orderId, paymentId) => {
     
     <body>
         <div class="container">
-            <a href="https://studynotion-edtech-project.vercel.app"><img class="logo" src="https://i.ibb.co/7Xyj3PC/logo.png"
+            <a href="https://edtech-studynotion-three.vercel.app"><img class="logo" src="https://i.ibb.co/7Xyj3PC/logo.png"
                     alt="studynotion Logo"></a>
             <div class="message">Course Payment Confirmation</div>
             <div class="body">
