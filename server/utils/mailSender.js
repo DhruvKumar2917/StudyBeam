2dbb4f2acaeab27df2483789945ac1c891c2d930
DhruvKumar2917 <dhruvkumar6297@gmail.com>
2025-10-05 02:06:26 +0530

    again doing8

--- server/utils/mailSender.js
+++ server/utils/mailSender.js
@@ -110,7 +110,7 @@ const resend = new Resend(process.env.RESEND_API_KEY);
 const mailSender = async (email, title, body) => {
   try {
     const response = await resend.emails.send({
-      from: "noreply@studynotion-backend.com", // Must match your verified sender domain
+      from: "studynotion-backend.com", // Must match your verified sender domain
       to: email,
       subject: title,
       html: body,
